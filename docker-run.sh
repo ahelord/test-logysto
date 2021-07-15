6963ac876144020e541e0d636e16f8fe61023faa
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-07-14 23:21:08 -0500

    Corrige imagen de docker

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build --pull --rm -f "Dockerfile" -t msprofilenew:latest "."
-docker run --rm -it -d -p 8080:8080/tcp msprofilenew:latest
+docker build --pull --rm -f "Dockerfile" -t testlogysto:latest "."
+docker run --rm -it -d -p 8080:8080/tcp testlogysto:latest
