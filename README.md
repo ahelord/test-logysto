d7370f240cb1cb0ea0fd7cee229fca7d4b4c87f0
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-07-15 22:43:24 -0500

    Commit here geocodign

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # test-logysto
+- Correr con docker 
+``` sh docker-run.sh ```
 - Persistencia se maneja en memoria, cada vez que se reinicie se borra todo.
 - Endpoint signUp
 
@@ -24,3 +26,6 @@ http://localhost:8080/api/auth/refresh-token
   "refreshToken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhaGVsb3JkQGdtYWlsLmNvbSIsImlkIjoiYWhlbG9yZEBnbWFpbC5jb20iLCJpYXQiOjE2MjYzMjI1MTYsImV4cCI6MTYyNjMyMzQxNn0.AoJsvIgTi-NIEyY6KYWAPheQqgmQlLV6StP3GdtkMe0"
 }
 
+
+fexivil792@godpeed.com
+Q6xfqD86M8BY
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,48 +4,11 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const router = express.Router();
 const AuthRoute = require('./auth.route');
+const CoordinatesRoute = require('./coordinates.route');
 
 // Routes
+router.use('/coordinates', CoordinatesRoute);
 router.use('/auth', AuthRoute);
 router.get('/status', (req, res) => res.send('OK'));
-
-// Swagger set up
-const options = {
-	swaggerDefinition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'My express mongoose API',
-			version: '1.0.0',
-			description:
-          '',
-			contact: {
-				name: 'John Doe',
-				url: '',
-				email: 'example@example.com'
-			}
-		},
-		servers: [
-			{
-				url: '/api'
-			}
-		]
-	},
-	apis: ['./models/**.js','./routes/api/**.js'],
-
-};
-const swaggerSpecs = swaggerJsdoc(options);
-router.use('/docs', swaggerUi.serve);
-router.get(
-	'/docs',
-	swaggerUi.setup(swaggerSpecs, {
-		explorer: true
-	})
-);
-
-router.get('/docs.json', (req, res) => {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(swaggerSpecs);
-});
-
 
 module.exports = router;
